7132bac2e37b845875d17902f6281495c83f8230
Petr123qwerty123 <82055144+Petr123qwerty123@users.noreply.github.com>
2022-06-29 09:18:46 +0300

    Add files via upload

---
+++ client/TzClasses/CreateParams.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TzClasses
+{
+    public class CreateParams
+    {
+        string Name;
+        string StartTime;
+        string EndTime;
+
+
+        public CreateParams(string Name, string StartTime, string EndTime)
+        {
+            this.Name = Name;
+            this.StartTime = StartTime;
+            this.EndTime = EndTime;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public string GetStartTime()
+        {
+            return StartTime;
+        }
+
+        public string GetEndTime()
+        {
+            return EndTime;
+        }
+
+    }
+}
